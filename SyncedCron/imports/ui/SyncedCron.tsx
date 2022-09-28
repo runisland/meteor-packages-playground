a6e1b0be9d8661adbc6d8c3ee4eab2c0e4574a82
ghybs <ghybs1@gmail.com>
2022-09-28 19:39:35 +0400

    feat(SyncedCron): add module
    for corresponding package.

---
+++ SyncedCron/imports/ui/SyncedCron.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function SyncedCron() {
+  return (
+    <>
+      <h1>SyncedCron</h1>
+      <p>
+        Playground for{" "}
+        <a
+          href="https://github.com/percolatestudio/meteor-synced-cron"
+          target="_blank"
+        >
+          <code>littledata:synced-cron</code>
+        </a>{" "}
+        Meteor Atmosphere package.
+      </p>
+    </>
+  );
+}
--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Hello } from "./Hello";
+import { SyncedCron } from "/SyncedCron/imports/ui/SyncedCron";
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
+    <SyncedCron />
   </div>
 );
