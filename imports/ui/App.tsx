4f7bd13215b9c4b3a5dd6a5db0da180e20df4c0d
ghybs <ghybs1@gmail.com>
2022-09-28 19:07:46 +0400

    refactor(all): remove unused LinksCollection

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+import React from "react";
+import { Hello } from "./Hello";
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello />
-    <Info />
   </div>
 );
