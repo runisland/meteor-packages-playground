ba552781e8a563d1cc8493ac44ec5855072bf17d
ghybs <ghybs1@gmail.com>
2022-10-01 22:18:39 +0400

    feat(SyncedCron): add UI & server methods

--- SyncedCron/imports/ui/SyncedCron.tsx
+++ SyncedCron/imports/ui/SyncedCron.tsx
@@ -1,3 +1,5 @@
+import { Field, Form, Formik } from "formik";
+import { Meteor } from "meteor/meteor";
 import React from "react";
 
 export function SyncedCron() {
@@ -14,6 +16,87 @@ export function SyncedCron() {
         </a>{" "}
         Meteor Atmosphere package.
       </p>
+
+      <fieldset>
+        <legend>Add a job (log in server console)</legend>
+        <Formik
+          initialValues={{ jobName: "", scheduleText: "" }}
+          onSubmit={(values) =>
+            Meteor.call("syncedCronAdd", values.jobName, values.scheduleText)
+          }
+        >
+          <Form>
+            <label>
+              Job name
+              <Field name="jobName" placeholder="Job name..." />
+            </label>
+            <label>
+              Schedule text expression
+              <Field
+                name="scheduleText"
+                placeholder="Schedule text expression..."
+              />
+            </label>
+            <button type="submit">Add</button>
+          </Form>
+        </Formik>
+      </fieldset>
+
+      <fieldset>
+        <legend>Request next schedule time of a job</legend>
+        <Formik
+          initialValues={{ jobName: "" }}
+          onSubmit={(values) =>
+            Meteor.call(
+              "syncedCronNextScheduledAtDate",
+              values.jobName,
+              (err: Meteor.Error, val: Date) => {
+                if (err) {
+                  console.error(err);
+                  alert(err);
+                } else {
+                  console.log(val);
+                  alert(val);
+                }
+              }
+            )
+          }
+        >
+          <Form>
+            <label>
+              Job name
+              <Field name="jobName" placeholder="Job name..." />
+            </label>
+            <button type="submit">Request</button>
+          </Form>
+        </Formik>
+      </fieldset>
+
+      <fieldset>
+        <legend>Remove a job</legend>
+        <Formik
+          initialValues={{ jobName: "" }}
+          onSubmit={(values) => Meteor.call("syncedCronRemove", values.jobName)}
+        >
+          <Form>
+            <label>
+              Job name
+              <Field name="jobName" placeholder="Job name..." />
+            </label>
+            <button type="submit">Remove</button>
+          </Form>
+        </Formik>
+      </fieldset>
+
+      <button type="button" onClick={() => Meteor.call("syncedCronStart")}>
+        Start
+      </button>
+      <button type="button" onClick={() => Meteor.call("syncedCronStop")}>
+        Stop
+      </button>
+      <button type="button" onClick={() => Meteor.call("syncedCronPause")}>
+        Pause
+      </button>
     </>
   );
 }
---
+++ SyncedCron/server/methods.ts
@@ -0,0 +1,45 @@
+import { SyncedCron } from "meteor/littledata:synced-cron";
+import { Meteor } from "meteor/meteor";
+
+// SyncedCron can only be used server-side,
+// so the Methods implementation is also only on server
+Meteor.methods({
+  syncedCronAdd(jobName: string, scheduleText: string) {
+    console.log(
+      `SyncedCron add job name "${jobName}" with schedule text expression"${scheduleText}"`
+    );
+    SyncedCron.add({
+      name: jobName,
+      schedule(parser) {
+        return parser.text(scheduleText);
+      },
+      job,
+    });
+  },
+  syncedCronStart() {
+    console.log(`SyncedCron start`);
+    SyncedCron.start();
+  },
+  syncedCronNextScheduledAtDate(
+    ...args: Parameters<typeof SyncedCron.nextScheduledAtDate>
+  ) {
+    console.log(`SyncedCron nextScheduledAtDate for job name "${args[0]}"`);
+    return SyncedCron.nextScheduledAtDate(...args);
+  },
+  syncedCronRemove(...args: Parameters<typeof SyncedCron.remove>) {
+    console.log(`SyncedCron remove job name "${args[0]}"`);
+    SyncedCron.remove(...args);
+  },
+  syncedCronStop() {
+    console.log(`SyncedCron stop`);
+    SyncedCron.stop();
+  },
+  syncedCronPause() {
+    console.log(`SyncedCron pause`);
+    SyncedCron.pause();
+  },
+});
+
+function job() {
+  console.log("run scheduled job");
+}
