48f1d7e12459036632e067c1b0549979b61495cd
ghybs <ghybs1@gmail.com>
2022-10-08 07:42:26 +0400

    docs(SyncedCron): explain global type
    using tsconfig types or triple-slash directive.

--- SyncedCron/server/main.ts
+++ SyncedCron/server/main.ts
@@ -1,4 +1,14 @@
-/// <reference types="meteor-synced-cron" /> // Load the type file somewhere
-import { SyncedCron } from "meteor/littledata:synced-cron";
+// In case `tsconfig.compilerOptions.types` option is used
+// (like in the default `meteor create --typescript` template),
+// the global type declarations (like for Meteor and packages)
+// are not automatically included in the TS compilation.
+// See https://www.typescriptlang.org/tsconfig#types
+// Therefore either list them into `types` option
+// (e.g. `"types": ["node", "mocha", "meteor-synced-cron"]`),
+// or use a triple-slash directive like below
+// (just once anywhere in your project is enough)
+// See https://www.typescriptlang.org/docs/handbook/triple-slash-directives.html#-reference-types-
+
+/// <reference types="meteor-synced-cron" />
 
 console.log("SyncedCron main server");
